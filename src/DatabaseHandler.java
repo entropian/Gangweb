9091b5557e358bb114e011c85283481e51d2c7f8
unknown <fffff6@.(none)>
2012-02-08 21:36:25 -0700

    Added methods to DatabaseHandler for connecting and disconnecting.

--- src/DatabaseHandler.java
+++ src/DatabaseHandler.java
@@ -1,11 +1,51 @@
+import java.sql.*;
 
+/**
+ * Handles all interactions with the GangWeb database.
+ */
 public class DatabaseHandler {
+	private Connection connection = null;
+	
+	/**
+	 * Creates an instance of DatabaseHandler with an initialized connection.
+	 */
 	public DatabaseHandler(){
-		Connect();
+		this.connection = Connect();
 	}
 	
-	private void Connect(){
+	/**
+	 * Creates a connection with the GangWeb database.
+	 * @return The established connection.
+	 */
+	private Connection Connect(){
+		String url = "jdbc:mysql://gangweb.db.3018661.hostedresource.com:3306/gangweb";
+		String driver = "com.mysql.jdbc.Driver";
+		String username = "gangweb";
+		String password = "PassworD1";
 		
+		try{
+			Class.forName(driver);
+			connection = DriverManager.getConnection(url,username,password);
+			System.out.println("Database connection established.");
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			System.out.println("Error: Could not establish a database connection.");
+		}
 		
+		return(connection);
+	}
+
+	/**
+	 * Terminates the connection with the GangWeb database.
+	 */
+	public void Disconnect(){
+		try{
+			this.connection.close();
+			System.out.println("Database connection terminated.");
+		}
+		catch(Exception e){
+			System.out.println("Error: Could not terminate database connection.");
+		}
 	}
 }
---
+++ src/test.java
@@ -0,0 +1,7 @@
+public class test {
+	public static void main(String[] args) {
+		DatabaseHandler dbhandler = new DatabaseHandler();
+		dbhandler.Disconnect();
+	}
+
+}
